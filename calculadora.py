f8f4a851b321811501ac8e48cfb5204fa7d8af6b
TonyJ0711 <ajh.guaman@yavirac.edu.ec>
2026-06-04 13:45:33 -0500

    Error cambio

--- calculadora.py
+++ calculadora.py
@@ -6,7 +6,7 @@ def restar(a, b):
     return a - b
 
 def main():
-    print("=== Calculadora ===")
+    print("=== Calculadora v1 ===")
     print(f"5 + 3 = {sumar(5, 3)}")
     print(f"10 - 4 = {restar(10, 4)}")
 
